1be228c6a12ca4905007f2bc7bf2aa9af1bceb1d
JammyRaptor <37228043+JammyRaptor@users.noreply.github.com>
2020-01-20 19:39:26 +0000

    working without rules

--- grid.py
+++ grid.py
@@ -1,37 +1,42 @@
 import random, copy
+
+
 def generate_grid(w, h):
     g = []
-    row = [0]*w
+    row = [0] * w
     for i in range(h):
         g.append(copy.deepcopy(row))
 
     return g
 
+
 def get_square(grid, c, r):
     return grid[c][r]
 
+
 def place_bomb(grid):
-    r = random.randint(0, len(grid)-1)
-    c = random.randint(0, len(grid)-1)
+    r = random.randint(0, len(grid) - 1)
+    c = random.randint(0, len(grid) - 1)
     if get_square(grid, c, r) != '*':
         grid[c][r] = '*'
         add_numbers(grid, c, r)
     else:
         place_bomb(grid)
 
+
 def add_numbers(grid, c, r):
-    h = len(grid)-1
-    w = len(grid[0])-1
+    h = len(grid) - 1
+    w = len(grid[0]) - 1
     # center left
-    if r > 0 and grid[c][r-1] != '*':
-        grid[c][r-1] += 1
+    if r > 0 and grid[c][r - 1] != '*':
+        grid[c][r - 1] += 1
 
     # center right
-    if r < w and grid[c][r+1] != '*':
-        grid[c][r+1] += 1
+    if r < w and grid[c][r + 1] != '*':
+        grid[c][r + 1] += 1
 
     # upper left
-    if c > 0 and r > 0 and grid[c-1][r-1] != '*':
+    if c > 0 and r > 0 and grid[c - 1][r - 1] != '*':
         grid[c - 1][r - 1] += 1
 
     # upper middle
@@ -39,20 +44,21 @@ def add_numbers(grid, c, r):
         grid[c - 1][r] += 1
 
     # upper right
-    if c > 0 and r < w and grid[c-1][r + 1] != '*':
+    if c > 0 and r < w and grid[c - 1][r + 1] != '*':
         grid[c - 1][r + 1] += 1
 
     # lower left
-    if c < h and r > 0 and grid[c+1][r-1] != '*':
+    if c < h and r > 0 and grid[c + 1][r - 1] != '*':
         grid[c + 1][r - 1] += 1
 
     # lower middle
-    if c < h and grid[c+1][r] != '*':
+    if c < h and grid[c + 1][r] != '*':
         grid[c + 1][r] += 1
 
     # lower right
-    if c < h and r < w and grid[c+1][r+1] != '*':
+    if c < h and r < w and grid[c + 1][r + 1] != '*':
         grid[c + 1][r + 1] += 1
+
 
 def make_grid(grid_width, grid_height, num_of_bombs):
     grid = generate_grid(grid_width, grid_height)
@@ -60,6 +66,56 @@ def make_grid(grid_width, grid_height, num_of_bombs):
         place_bomb(grid)
     return grid
 
+
+def make_blank_grid(grid_width, grid_height, num_of_bombs):
+    grid = generate_grid(grid_width, grid_height)
+    return grid
+
+
+def snake(grid, visible):
+    v2 = copy.deepcopy(visible)
+    for c in range(len(grid)):
+        for r in range(len(grid[0])):
+            if visible[c][r] and grid[c][r] == 0:
+                visible = place_zeros(grid, visible, c, r)
+
+    if visible != v2:
+        snake(grid, visible)
+def place_zeros(grid, v, c, r):
+    h = len(grid) - 1
+    w = len(grid[0]) - 1
+    # center left
+    if r > 0:
+        v[c][r - 1] = 1
+
+    # center right
+    if r < w:
+        v[c][r + 1] = 1
+
+    # upper left
+    if c > 0 and r > 0:
+        v[c - 1][r - 1] = 1
+
+    # upper middle
+    if c > 0:
+        v[c - 1][r] = 1
+
+    # upper right
+    if c > 0 and r < w:
+        v[c - 1][r + 1] = 1
+
+    # lower left
+    if c < h and r > 0:
+        v[c + 1][r - 1] = 1
+
+    # lower middle
+    if c < h:
+        v[c + 1][r] = 1
+
+    # lower right
+    if c < h and r < w:
+        v[c + 1][r + 1] = 1
+    return v
 if __name__ == '__main__':
     grid_width = 16
     grid_height = 16
@@ -69,7 +125,6 @@ if __name__ == '__main__':
 
     for i in range(num_of_bombs):
         place_bomb(grid)
-
 
     for line in grid:
         l = ''
--- main.py
+++ main.py
@@ -1,9 +1,62 @@
-from grid import make_grid
-import pygame
+from grid import make_grid, make_blank_grid, snake
+import pygame as pg
+
+
+pg.init()
+win = pg.display.set_mode((800, 800))
+
+tile = pg.transform.scale(pg.image.load('./assets/tile.png'), (50, 50))
+bgtile = pg.transform.scale(pg.image.load('./assets/bg_tile.png'), (50, 50))
+flag = pg.transform.scale(pg.image.load('./assets/flag.png'), (50, 50))
+
+font = pg.font.Font('freesansbold.ttf', 50)
+fontcols = {1: (0, 0, 255), 2: (0, 128, 0), 3: (255, 0, 0), 4: (2, 13, 113), 5: (77, 0, 0), 6: (0, 139, 139),
+            7: (0, 0, 0), 8: (131, 131, 131), '*': (255, 0, 0)}
 
 g = make_grid(16, 16, 40)
-for line in g:
-    l = ''
-    for square in line:
-        l = f'{l}  {square}'
-    print(l)
+g2 = make_blank_grid(16, 16, 40)
+flags = make_blank_grid(16, 16, 40)
+
+def printgrid():
+    for c in range(0, 16):
+        for r in range(0, 16):
+
+            win.blit(bgtile, (c * 50, r * 50))
+            if (t := g[c][r]) != 0:
+                win.blit(font.render(f'{t}', True, fontcols[t]), ((c * 50) + 10, (r * 50) + 2))
+            if not g2[c][r]:
+                win.blit(tile, (c * 50, r * 50))
+            if flags[c][r]:
+                win.blit(flag, (c * 50, r * 50))
+    pg.display.update()
+
+
+while True:
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+        if event.type == pg.MOUSEBUTTONDOWN:
+
+            if pg.mouse.get_pressed()[0]:
+                l1, l2 = pg.mouse.get_pos()
+                l1, l2 = (l1//50, l2//50)
+                if not flags[l1][l2]:
+                    g2[l1][l2] = 1
+                    if g[l1][l2] == 0:
+                        snake(g, g2)
+            elif pg.mouse.get_pressed()[2]:
+                l1, l2 = pg.mouse.get_pos()
+                l1, l2 = (l1 // 50, l2 // 50)
+                if not flags[l1][l2]:
+                    flags[l1][l2] = 1
+                else:
+                    flags[l1][l2] = 0
+
+    printgrid()
+
+# for line in g:
+#    l = ''
+#    for square in line:
+#        l = f'{l}  {square}'
+#    print(l)
