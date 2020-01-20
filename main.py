43e1d2824b9e09d30db1b9528e5dad715ac2ccae
harry <harry.crane@outlook.com>
2020-01-20 21:23:19 +0000

    added clock
    if you tap on a bomb, all bombs are reveiled

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ win = pg.display.set_mode((800, 800))
 tile = pg.transform.scale(pg.image.load('./assets/tile.png'), (50, 50))
 bgtile = pg.transform.scale(pg.image.load('./assets/bg_tile.png'), (50, 50))
 flag = pg.transform.scale(pg.image.load('./assets/flag.png'), (50, 50))
+mine = pg.transform.scale(pg.image.load('./assets/mine.png'), (50, 50))
 
 font = pg.font.Font('freesansbold.ttf', 50)
 fontcols = {1: (0, 0, 255), 2: (0, 128, 0), 3: (255, 0, 0), 4: (2, 13, 113), 5: (77, 0, 0), 6: (0, 139, 139),
@@ -17,13 +18,18 @@ g = make_grid(16, 16, 40)
 g2 = make_blank_grid(16, 16, 40)
 flags = make_blank_grid(16, 16, 40)
 
+clock = pg.time.Clock()
+
 def printgrid():
-    for c in range(0, 16):
-        for r in range(0, 16):
+    for c in range(0, len(g)):
+        for r in range(0, len(g[0])):
 
             win.blit(bgtile, (c * 50, r * 50))
             if (t := g[c][r]) != 0:
-                win.blit(font.render(f'{t}', True, fontcols[t]), ((c * 50) + 10, (r * 50) + 2))
+                if t == '*':
+                    win.blit(mine, (c * 50, r * 50))
+                else:
+                    win.blit(font.render(f'{t}', True, fontcols[t]), ((c * 50) + 10, (r * 50) + 2))
             if not g2[c][r]:
                 win.blit(tile, (c * 50, r * 50))
             if flags[c][r]:
@@ -32,7 +38,7 @@ def printgrid():
 
 
 while True:
-
+    clock.tick(25)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
@@ -41,6 +47,12 @@ while True:
             if pg.mouse.get_pressed()[0]:
                 l1, l2 = pg.mouse.get_pos()
                 l1, l2 = (l1//50, l2//50)
+                if g[l1][l2] == '*':
+                    for c in range(0, len(g)):
+                        for r in range(0, len(g[0])):
+                            if g[c][r] == '*':
+                                g2[c][r] = 1
+                                flags[c][r] = 0
                 if not flags[l1][l2]:
                     g2[l1][l2] = 1
                     if g[l1][l2] == 0:
